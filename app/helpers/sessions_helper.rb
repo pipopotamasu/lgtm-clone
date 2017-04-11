fe2f0e7b4e106d2a9e0e9c307dd2b060f83ec43b
pipopotamasu <ec0156hx@gmail.com>
2017-04-11 21:26:02 +0100

    sign up画面がひらけないバグを修正

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -16,7 +16,7 @@ module SessionsHelper
 
   # 現在ログイン中のユーザーを返す (いる場合)
   def current_user
-    if (session[:user_id])
+    if session[:user_id]
       # @current_user = @current_user || User.find_by(id: session[:user_id])と同じ
       @current_user ||= User.find_by(id: session[:user_id])
     elsif (user_id = cookies.signed[:user_id])
--- app/uploaders/image_uploader.rb
+++ app/uploaders/image_uploader.rb
@@ -54,5 +54,4 @@ class ImageUploader < CarrierWave::Uploader::Base
   # def filename
   #   "something.jpg" if original_filename
   # end
-
 end
